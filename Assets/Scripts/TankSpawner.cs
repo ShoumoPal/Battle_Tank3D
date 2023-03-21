214b5ec093eb7b86ff63ced246a3621c40ee1072
ShoumoPal <arka.pal21@gmail.com>
2023-03-21 16:04:23 +0530

    Added MVC implementation

--- Assets/Scripts/TankSpawner.cs
+++ Assets/Scripts/TankSpawner.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class TankSpawner : MonoBehaviour
 {
-    public GameObject tankPrefab;
+    public TankView tankPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(tankPrefab, transform.position, Quaternion.identity);
+        CreateTank();
+    }
+
+    private void CreateTank()
+    {
+        TankModel tankModel = new TankModel();
+        TankController tankController = new TankController(tankPrefab, tankModel);
     }
 }
